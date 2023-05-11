5814d90926aa3841b04ae0c25aa4a02bffd71bb9
SurajPrakash <prakash.champ999@gmail.com>
2023-05-11 16:16:58 +0530

    Changes | calling of a functions

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
+
+        int overloading_1 = p.product(4, 5);
+        int overloading_2 = p.product(6, 7, 8);
+        double overloading_3 = p.product(9.5, 10.5);
+        System.out.println("first method- "+overloading_1+" Second method- "+overloading_2+" Third method- "+overloading_3);
+
     }
 public static class Product{
     public int product(int x, int y){
